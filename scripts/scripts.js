8957a7e913ddbe22d1170507d65701f7321837a5
Jeremy Blackmore <jeremyblackmore@iMac.local>
2020-11-04 20:05:01 +0000

    This is the CSS version

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,6 @@
+"use strict";
 (() => {
+    let origianlData = [];
     fetch('../files.json')
         .then(respone => respone.json())
         .then(data => {
@@ -21,35 +23,47 @@
         if (data.length > 0) {
             data.forEach(file => {
                 if (file.type === 'folder') {
-                    html += `<div class="folder">
-                                <h3>${file.name.toUpperCase()}</h3>
-                                <p>${file.type.toUpperCase()}</p>
-                                ${file.added? `<p>${file.added}</p>` : ''}
-                                <button id=${id + 10} class="openAccordion">Open Folder</button>
+                    html += ` <div class="document">
+                                <div class="document_details">
+                                    <h3>${file.name.toUpperCase()}</h3>
+                                    <p>${file.type.toUpperCase()}</p>
+                                    ${file.added? `<p>${file.added}</p>` : ''}
+                                </div>
+                                <div class="document_buttons">
+                                    <button id=${id + 10} class="openAccordion btn">Open Folder</button>
+                                </div>
                                 <div class="hide accordion" id=${id}>
-                                ${file.files.map(subFile => {
-                                    return `<div class="file">
-                                                <h3>${subFile.name.toUpperCase()}</h3>
-                                                <p>${subFile.type.toUpperCase()}</p>
-                                                <p>${subFile.added}</p>
-                                                <button><a href="https://www.jezblackmore.com/JezBlackmoreBScMA.pdf" target="_blank">View File</a></button>
-                                            </div>`
-                                })}
+                                    ${file.files.map(subFile => {
+                                        return `<div class="document">
+                                                     <div class="document_details">
+                                                        <h3>${subFile.name.toUpperCase()}</h3>
+                                                        <p>${subFile.type.toUpperCase()}</p>
+                                                        <p>${subFile.added}</p>
+                                                    </div>
+                                                    <div class="document_buttons">
+                                                        <a href="https://www.jezblackmore.com/JezBlackmoreBScMA.pdf" target="_blank"><button class="btn">View File</button></a>
+                                                    </div>
+                                                </div>`
+                                    })}
                                 </div>
                             </div>`
                 } else {
-                    html += `<div class="file">
+                    html += `<div class="document">
+                                <div class="document_details">
                                     <h3>${file.name.toUpperCase()}</h3>
                                     <p>${file.type.toUpperCase()}</p>
                                     <p>${file.added}</p>
-                                    <button><a href="https://www.jezblackmore.com/JezBlackmoreBScMA.pdf" target="_blank">View File</a></button>
-                                </div>`
+                                </div>
+                                <div class="document_buttons">
+                                    <a href="https://www.jezblackmore.com/JezBlackmoreBScMA.pdf" target="_blank"><button class="btn">View File</button></a>
+                                </div>
+                            </div>`
                 }
 
                 id += 1;
             })
         } else {
-            html = `<p>Sorry, no matching results</p>`
+            html = `<h2 class="noResults">Sorry, no matching results</h2>`
         }
 
         results.innerHTML = html;
@@ -80,7 +94,7 @@
 
         searchInput.addEventListener('input', (e) => {
             let term = e.target.value;
-            search(term);
+            search(term.toLowerCase());
         })
 
         filter.addEventListener('change', (e) => {
@@ -95,9 +109,7 @@
             return;
         }
         let data = [...origianlData]
-        let newData = data.filter(file => {
-            return file.name.toLowerCase().includes(searchTerm);
-        })
+        let newData = data.filter(file => file.name.toLowerCase().includes(searchTerm))
 
         updateHTML(newData);
     }
@@ -111,8 +123,7 @@
             const file1 = a[property].toLowerCase();
             const file2 = b[property].toLowerCase();
 
-            let comparison = 0;
-            (file1 > file2) ? comparison = 1: comparison = -1;
+            let comparison = (file1 > file2) ? 1:  -1;
             return ascOrDsc === "+" ? comparison : comparison * -1;
         }
 
@@ -122,4 +133,3 @@
 
 })();
 
-let origianlData = [];
